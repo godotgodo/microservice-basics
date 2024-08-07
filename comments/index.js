b2542fbcc78e23663c5be202b3fd154a0d053bb3
godotgodo <yunusemregolbasi0@gmail.com>
2024-08-08 00:58:50 +0300

    feat(syncing) add syncing operations for some services goes down

--- comments/index.js
+++ comments/index.js
@@ -25,7 +25,8 @@ app.post('/posts/:id/comments',async(req,res)=>{
     const data={
         id:commentId,
         content,
-        postId:req.params.id
+        postId:req.params.id,
+        status:"pending"
     }
     await axios.post('http://localhost:4005/events',{
         type:'CommentCreated',
@@ -34,8 +35,26 @@ app.post('/posts/:id/comments',async(req,res)=>{
     res.status(201).send(comments)
 });
 
-app.post('/events',(req,res)=>{
+app.post('/events',async(req,res)=>{
     console.log('Event Received',req.body.type)
+
+    const { type, data }= req.body
+
+    if (type==='CommentModerated') {
+        const {postId,id,status,content}=data;
+        const comments= commentsByPostId[postId];
+        const comment=comments.find(comment=>comment.id===id)    
+        comment.status=status
+        await axios.post('http://localhost:4005/events',{
+            type:'CommentUpdated',
+            data:{
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
     res.send({})
 })
 
--- query/index.js
+++ query/index.js
@@ -2,9 +2,35 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors');
 const app=express();
+const axios=require("axios");
 
 app.use(bodyParser.json());
 app.use(cors());
+
+const handleEvent=(type,data)=>{
+    if(type==='PostCreated'){
+        const {id,title}=data;
+        posts[id]={id,title,comments:[]};
+    }
+
+    if(type==='CommentCreated'){
+        const {id,content,postId,status}=data;
+        const post=posts[postId];
+        post.comments.push({id,content,status})
+    }
+
+    if (type==='CommentUpdated') {
+        const {id,content,postId,status}=data;
+        const post=posts[postId];
+
+        const comment=post.comments.find(comment=>{
+            return comment.id===id;
+        })
+
+        comment.status=status;
+        comment.content=content;
+    }
+}
 
 const posts={};
 
@@ -15,18 +41,18 @@ app.get('/posts',(req,res)=>{
 app.post('/events',(req,res)=>{
     const { type, data }= req.body;
     
-    if(type==='PostCreated'){
-        const {id,title}=data;
-        posts[id]={id,title,comments:[]};
-    }
-    if(type==='CommentCreated'){
-        const {id,content,postId}=data;
-        const post=posts[postId];
-        post.comments.push({id,content})
-    }
+    handleEvent(type,data)
+
     res.send({});
 })
 
-app.listen(4002,(req,res)=>{
+app.listen(4002,async(req,res)=>{
     console.log("Listening on 4002");
+
+    const getEventsResponse = await axios.get('http://localhost:4005/events')
+    for (let event of getEventsResponse.data) {
+        console.log("Processing Event: ",event.type);
+        const {type,data}=event;
+        handleEvent(type,data);
+    }
 })
